66aef7f614c44622ee2b34d283e8effd05e79efc
ebenezereby1 <ebenezerthiru1@gmail.com>
2025-12-23 12:47:39 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -21,7 +21,7 @@ module "eks" {
   eks_managed_node_groups = {
     default = {
       instance_types = ["t3.micro"]
-      desired_size   = 1
+      desired_size   = 2
       min_size       = 1
       max_size       = 2
     }
