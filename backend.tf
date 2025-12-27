eb4929e17ca633d3bd6797c59714f21241800a87
ebenezereby1 <ebenezerthiru1@gmail.com>
2025-12-27 16:59:20 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "devops-hackathon-123456789012-ap-south-1-tf-state"
-    key            = "eks/dev/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
-}
